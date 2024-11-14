92dfac9d0a725b16e31999440517e42832dc5b8d
muhammed ali <muhammedali54254715@gmail.com>
2024-11-14 23:05:05 +0200

    Adding projects

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,16 @@
+import UserContext from './UserContext'; 
+import React from 'react';
+
+
+ const UserProfile = () => {
+  const userData = useContext(UserContext); 
+   return (
+     <div>
+       <h2>{userData.name}</h2>
+       <p>Age: {userData.age}</p>
+       <p>Bio: {userData.bio}</p>
+     </div>
+   );
+ };
+
+ export default UserProfile;
