9474c27996ecdf7d54c4c676c7a8e1cebe7b32a1
muhammed ali <muhammedali54254715@gmail.com>
2024-11-14 23:10:46 +0200

    Adding flex display

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,13 +1,31 @@
 // src/Navbar.jsx
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#eee', marginBottom: '20px' }}>
-      <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px",
+        backgroundColor: "#333",
+        color: "white",
+      }}
+    >
+      <div>
+        <Link to="/" style={{ color: "white", marginRight: "15px" }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ color: "white", marginRight: "15px" }}>
+          About
+        </Link>
+        <Link to="/services" style={{ color: "white", marginRight: "15px" }}>
+          Services
+        </Link>
+        <Link to="/contact" style={{ color: "white" }}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
