7652958d572788d07de75e1ea89505bf0e1f00da
muhammed ali <muhammedali54254715@gmail.com>
2024-11-24 13:33:18 +0200

    creating zustand and react

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  );
+}
+
+export default App;
