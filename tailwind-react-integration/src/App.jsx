ec2c416fd4bdac10c607363cb19cb4f8e2775b0c
muhammed ali <muhammedali54254715@gmail.com>
2024-12-06 05:11:19 +0200

    Adding Styling and userProfile card

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,17 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Tailwind CSS is working!
-      </h1>
-    </div>
-  );
+  return <UserProfile />;
 }
 
 export default App;
