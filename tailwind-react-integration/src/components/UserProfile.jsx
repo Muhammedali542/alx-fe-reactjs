7d63a62d8858bc8c64f0c439c8da9eeed95e2dc3
muhammed ali <muhammedali54254715@gmail.com>
2024-12-07 08:09:32 +0200

    Adding Advanced Interactivity

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg xl:p-12 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg xl:p-12 md:p-8 sm:p-4 md:max-w-sm sm:max-w-xs hover:shadow-xl">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        class="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full"
+        class="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 class=" my-4 text-lg md:text-xl text-blue-800 ">John Doe</h1>
-      <p class="text-gray-600 text-sm md:text-base">
+      <h1 class=" my-4 text-lg md:text-xl text-blue-800  hover:text-blue-500">
+        John Doe
+      </h1>
+      <p class="text-gray-600 text-sm md:text-base ">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
